d5e466b20e8a7f7430f90c0a57b85580ad53b809
Sahed <sahed.moral@123rf.com>
2023-07-11 23:43:26 +0600

    Digit Sum Challenge

--- src/DigitSum.java
+++ src/DigitSum.java
@@ -10,6 +10,9 @@ public class DigitSum {
      * @return sum of the integer
      */
     public static int extractNumber(int number){
+        if (number<0){
+            return -1;
+        }
         int sum = 0;
         while (number>0){
             // find the right utmost digit
