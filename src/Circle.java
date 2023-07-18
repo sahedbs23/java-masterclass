003371878fedd6f9133fe35b31a04fa5aa7ae415
Sahed <sahed.moral@123rf.com>
2023-07-19 00:57:03 +0600

    Ex# 4590579

---
+++ src/Circle.java
@@ -0,0 +1,18 @@
+public class Circle {
+    // write your code here
+    private double radius;
+
+    public Circle(double radius){
+        this.radius = radius<0?0:radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea(){
+        return radius * radius * Math.PI;
+    }
+
+
+}
---
+++ src/Cylinder.java
@@ -0,0 +1,16 @@
+public class Cylinder extends Circle{
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume(){
+        return this.getArea() * this.height;
+    }
+}
